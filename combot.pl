89cf8e07b8da4bfed0d1905cb6c3bf31f03cd1ab
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2014-10-11 19:38:43 +0200

    update bot settings

--- combot.pl
+++ combot.pl
@@ -4,11 +4,11 @@ my $bot = Combot->new(
 	server => "irc.freenode.org",
 	port => 7000,
 	ssl => 1,
-	channels => ['#testhaum'],
+	channels => ['#haum'],
 	nick => 'Com`bot',
 	redis_db => 3,
 	redis_pref => "combot:",
 	master => "matael",
-	agenda_db => "/home/matael/mergedbot/agenda_test.sqlite"
+	agenda_db => "/home/haum/website/agenda.sqlite"
 )->run();
 
