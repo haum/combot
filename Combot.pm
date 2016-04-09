89e4ad29c1302554b96c5a2c2681d472f1bf5686
Romuald Conty <neomilium@gmail.com>
2016-04-09 22:42:10 +0200

    Do not use bad connoted words

--- Combot.pm
+++ Combot.pm
@@ -35,7 +35,6 @@ sub init {
 		"Si pour 2016 t'as des projets, viens nous voir et nous en parler !",
 		"Si tu aimes bricoler, viens t'amuser avec nous !",
 		"Tant que tu n'as pas essayé tu peux encore te passer de nous... VIENS !",
-		"Curieux de nature ? Tu trouveras ta place dans notre secte ;)",
 		"Si hacker est pour toi plus qu'un truc qu'on entend aux infos, passe nous voir !",
 		"Envie de plancher avec nous à d'autres expériences insolites ?"
 	];
