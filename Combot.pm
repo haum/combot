e02303bc3e671ae73891f708c7ecbb040fc0dd74
Fred (feedoo) Blain <f.blain@sheffield.ac.uk>
2017-01-04 10:42:23 +0000

    Combot.pm is now up-to-date!

--- Combot.pm
+++ Combot.pm
@@ -32,7 +32,7 @@ sub init {
 	];
 
 	$self->{agenda_messages} = [
-		"Si pour 2016 t'as des projets, viens nous voir et nous en parler !",
+		"Si pour 2017 t'as des projets, viens nous voir et nous en parler !",
 		"Si tu aimes bricoler, viens t'amuser avec nous !",
 		"Tant que tu n'as pas essayé tu peux encore te passer de nous... VIENS !",
 		"Si hacker est pour toi plus qu'un truc qu'on entend aux infos, passe nous voir !",
